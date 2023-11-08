6980154fe56bf3139edcf9b9eb86ab67ca69cc93
Eileen <eileenburdekin@proton.me>
2023-11-08 10:30:51 -0800

    added bonus deliverable 1

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ function renderDetails(cakeObj) {
     const desc = document.querySelector('#cake-description')
     const reviewList = document.querySelector('#review-list')
     reviewList.innerHTML = ''
-    console.log('clicked')
     name.textContent = cakeObj.name
     image.src = cakeObj.image_url
     desc.textContent = cakeObj.description
@@ -39,11 +38,13 @@ function renderDetails(cakeObj) {
     reviews.forEach(review => {
         const reviewLi = document.createElement('li')
         reviewLi.textContent = review
+        reviewLi.id = cakeObj.id
+        reviewLi.addEventListener('click', handleDelete)
         reviewList.append(reviewLi)
     })
 }
 
-//Form setup
+//Review form setup
 
 const form = document.querySelector('#review-form')
 
@@ -51,8 +52,43 @@ form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(e) {
     e.preventDefault()
+    //POST REQUEST
+
+
+    //Add .then before below:
     const reviewList = document.querySelector('#review-list')
     const reviewLi = document.createElement('li')
     reviewLi.textContent = e.target.review.value
+    reviewLi.addEventListener('click', handleDelete)
     reviewList.append(reviewLi)
+}
+
+
+//Function to delete comment when clicked
+
+function handleDelete(e){
+    e.target.remove()
+}
+
+//Functions to handle description submit
+const descForm = document.querySelector('#description-form')
+descForm.addEventListener('submit', handleDescSubmit)
+
+function handleDescSubmit(e) {
+    e.preventDefault()
+    const newDesc = e.target.description.value
+    const desc = document.querySelector('#cake-description')
+    //PATCH REQUEST
+
+    // fetch(`${cakeAPI}/${cakeObj.id}`, {
+    //     method: 'PATCH',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(newDesc)
+    // })
+    // .then(res => res.json())
+    // .then(() => {
+    //     desc.textContent = newDesc})
+    desc.textContent = newDesc
 }
